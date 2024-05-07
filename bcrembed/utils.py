aefc0490190fd7730bcd2da26b712bfa8025d2b0
Mamie Wang <mamie.wang@yale.edu>
2024-05-07 16:41:19 -0400

    Refactored the translation function

--- bcrembed/utils.py
+++ bcrembed/utils.py
@@ -1,5 +1,7 @@
 """Main module."""
 import pandas as pd
+import os
+import subprocess
 
 def batch_loader(data, batch_size):
     num_samples = len(data)
@@ -23,51 +25,59 @@ def pivot_airr(inpath):
     data.loc[:,'HL'] = data.H + '<cls><cls>' + data.L
     return data
 
-def translate_igblast(inpath):
-    command_igblastn = ["igblastn", "-germline_db_V", "../databases/igblast_base/database/imgt_human_ig_v",
-           "-germline_db_D", "../databases/igblast_base/database/imgt_human_ig_d",
-           "-germline_db_J", "../databases/igblast_base/database/imgt_human_ig_j",
-           "-query", "fasta",
+def translate_igblast(inpath: str, outdir: str, reference_dir: str):
+    """
+    Translates nucleotide sequences to amino acid sequences using IgBlast.
+
+    This function takes a AIRR file containing nucleotide sequences
+    and translates them into amino acid sequences using IgBlast, a tool for analyzing
+    immunoglobulin and T cell receptor sequences. It performs the following steps:
+
+    1. Reads the input TSV file containing nucleotide sequences.
+    2. Writes the nucleotide sequences into a FASTA file, required as input for IgBlast.
+    3. Runs IgBlast on the FASTA file to perform sequence alignment and translation.
+    4. Reads the IgBlast output, which includes the translated amino acid sequences.
+    5. Removes gaps introduced by IgBlast from the sequence alignment.
+    6. Saves the translated data into a new TSV file in the specified output directory.
+
+    Args:
+        inpath (str): Path to the input TSV file containing nucleotide sequences.
+        outdir (str): Directory to save the translated output files.
+        reference_dir (str): Directory with reference for igblast
+    """
+    data = pd.read_csv(inpath, sep="\t")
+    out_fasta = os.path.join(outdir, os.path.splitext(os.path.basename(inpath))[0]+".fasta")
+    out_igblast = os.path.join(outdir, os.path.splitext(os.path.basename(inpath))[0]+"_igblast.tsv")
+    out_translated = os.path.join(outdir, os.path.splitext(os.path.basename(inpath))[0]+"_translated.tsv")
+
+    # Write out FASTA file
+    with open(out_fasta, "w") as f:
+        for _, row in data.iterrows():
+            f.write(">" + row["sequence_id"] + "\n")
+            f.write(row["sequence"] + "\n")
+    
+    # Run IgBlast on FASTA
+    command_igblastn = ["igblastn", "-germline_db_V", f"{reference_dir}/imgt_human_ig_v",
+           "-germline_db_D", f"{reference_dir}/imgt_human_ig_d",
+           "-germline_db_J", f"{reference_dir}/imgt_human_ig_j",
+           "-query", out_fasta,
            "-organism", "human",
-           "-auxiliary_data", "../databases/igblast_base/database/human_gl.aux",
+           "-auxiliary_data", f"{reference_dir}/human_gl.aux",
            "-show_translation",
            "-outfmt", "19",
-           "-out", "tsv"]
+           "-out", out_igblast]
+    subprocess.run(command_igblastn)
 
-    for file in rep_files:
-        rep = pd.read_csv(file, sep="\t")
-        filename = os.path.basename(file)
-        filename_without_extension = os.path.splitext(filename)[0]
-        print(filename_without_extension)
+    # Read IgBlast output
+    igblast_transl = pd.read_csv(out_igblast, sep="\t", usecols=["sequence_id","sequence_aa", "sequence_alignment_aa"])
 
-        with open(outdir + filename_without_extension +".fasta", "w") as f:
-            for i in range(len(rep)):
-                f.write(">" + rep["sequence_id"].iloc[i] + "\n")
-                f.write(rep["sequence"].iloc[i] + "\n")
+    # Remove IMGT gaps
+    sequence_vdj_aa = [ sa.replace("-","") for sa in igblast_transl["sequence_alignment_aa"]]
+    igblast_transl["sequence_vdj_aa"] = sequence_vdj_aa
 
-    fastas = [os.path.splitext(os.path.basename(file))[0]+".fasta" for file in rep_files]
-    tsvs = [os.path.splitext(os.path.basename(file))[0]+"_igblast.tsv" for file in rep_files]
-
-    for (fasta,tsv) in zip(fastas,tsvs):
-        print(fasta,tsv)
-        command_igblastn[-1] = outdir + tsv
-        command_igblastn[-10] = outdir + fasta
-        print(command_igblastn)
-        subprocess.run(command_igblastn)
-
-    for (file,transl) in zip(rep_files,tsvs):
-        rep = pd.read_csv(file, sep="\t")
-        #igblast_transl = pd.read_csv(outdir+transl, sep="\t", usecols=["sequence_id","sequence_aa"])
-        igblast_transl = pd.read_csv(outdir+transl, sep="\t", usecols=["sequence_id","sequence_aa", "sequence_alignment_aa"])
-        sequence_vdj_aa = [ sa.replace("-","") for sa in igblast_transl["sequence_alignment_aa"]]
-        igblast_transl["sequence_vdj_aa"] = sequence_vdj_aa
-        rep_transl = pd.merge(rep, igblast_transl, on="sequence_id", how="left")
-        rep_transl.to_csv(outdir+os.path.splitext(os.path.basename(file))[0]+"_translated.tsv", sep="\t", index=False)
-        rep_transl.head()
-
-
-
-
+    # Merge and save the translated data with original data
+    data_transl = pd.merge(data, igblast_transl, on="sequence_id", how="left")
+    data_transl.to_csv(out_translated, sep="\t", index=False)
 
 # test case:
 # dat = pivot_airr("/gpfs/gibbs/pi/kleinstein/embeddings/example_data/single_cell/MG-1__clone-pass_translated.tsv")
