f3dfc94f6dca9d7085236dffb27e45997b7c842d
Gisela Gabernet <gisela.gabernet@gmail.com>
2024-05-07 15:58:03 -0400

    add translation raw code

--- bcrembed/utils.py
+++ bcrembed/utils.py
@@ -23,5 +23,51 @@ def pivot_airr(inpath):
     data.loc[:,'HL'] = data.H + '<cls><cls>' + data.L
     return data
 
+def translate_igblast(inpath):
+    command_igblastn = ["igblastn", "-germline_db_V", "../databases/igblast_base/database/imgt_human_ig_v",
+           "-germline_db_D", "../databases/igblast_base/database/imgt_human_ig_d",
+           "-germline_db_J", "../databases/igblast_base/database/imgt_human_ig_j",
+           "-query", "fasta",
+           "-organism", "human",
+           "-auxiliary_data", "../databases/igblast_base/database/human_gl.aux",
+           "-show_translation",
+           "-outfmt", "19",
+           "-out", "tsv"]
+
+    for file in rep_files:
+        rep = pd.read_csv(file, sep="\t")
+        filename = os.path.basename(file)
+        filename_without_extension = os.path.splitext(filename)[0]
+        print(filename_without_extension)
+
+        with open(outdir + filename_without_extension +".fasta", "w") as f:
+            for i in range(len(rep)):
+                f.write(">" + rep["sequence_id"].iloc[i] + "\n")
+                f.write(rep["sequence"].iloc[i] + "\n")
+
+    fastas = [os.path.splitext(os.path.basename(file))[0]+".fasta" for file in rep_files]
+    tsvs = [os.path.splitext(os.path.basename(file))[0]+"_igblast.tsv" for file in rep_files]
+
+    for (fasta,tsv) in zip(fastas,tsvs):
+        print(fasta,tsv)
+        command_igblastn[-1] = outdir + tsv
+        command_igblastn[-10] = outdir + fasta
+        print(command_igblastn)
+        subprocess.run(command_igblastn)
+
+    for (file,transl) in zip(rep_files,tsvs):
+        rep = pd.read_csv(file, sep="\t")
+        #igblast_transl = pd.read_csv(outdir+transl, sep="\t", usecols=["sequence_id","sequence_aa"])
+        igblast_transl = pd.read_csv(outdir+transl, sep="\t", usecols=["sequence_id","sequence_aa", "sequence_alignment_aa"])
+        sequence_vdj_aa = [ sa.replace("-","") for sa in igblast_transl["sequence_alignment_aa"]]
+        igblast_transl["sequence_vdj_aa"] = sequence_vdj_aa
+        rep_transl = pd.merge(rep, igblast_transl, on="sequence_id", how="left")
+        rep_transl.to_csv(outdir+os.path.splitext(os.path.basename(file))[0]+"_translated.tsv", sep="\t", index=False)
+        rep_transl.head()
+
+
+
+
+
 # test case:
 # dat = pivot_airr("/gpfs/gibbs/pi/kleinstein/embeddings/example_data/single_cell/MG-1__clone-pass_translated.tsv")
