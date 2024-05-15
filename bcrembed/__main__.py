bded03ae31dafdbacdf7d2db92a3ceeb5a765a5e
Mamie Wang <mamie.wang@yale.edu>
2024-05-15 10:15:57 -0400

    Moved the translate function to CLI

--- bcrembed/__main__.py
+++ bcrembed/__main__.py
@@ -312,6 +312,71 @@ def custommodel(modelpath: Annotated[str, typer.Argument(..., help= 'The path to
     save_embedding(dat, embeddings, outpath)
     logger.info("Saved embedding at %s", outpath)
 
+@app.command()
+def translate_igblast(inpath: Annotated[str, typer.Argument(..., help= 'The path to the input data file. The data file should be in AIRR format.')],
+                      outdir: Annotated[str, typer.Argument(..., help= 'The directory where the generated embeddings will be saved.')],
+                      reference_dir: Annotated[str, typer.Argument(..., help= 'The directory to the igblast references.')]):
+    """
+    Translates nucleotide sequences to amino acid sequences using IgBlast.
+
+    This function takes a AIRR file containing nucleotide sequences
+    and translates them into amino acid sequences using IgBlast, a tool for analyzing
+    immunoglobulin and T cell receptor sequences. It performs the following steps:
+
+    1. Reads the input TSV file containing nucleotide sequences.
+    2. Writes the nucleotide sequences into a FASTA file, required as input for IgBlast.
+    3. Runs IgBlast on the FASTA file to perform sequence alignment and translation.
+    4. Reads the IgBlast output, which includes the translated amino acid sequences.
+    5. Removes gaps introduced by IgBlast from the sequence alignment.
+    6. Saves the translated data into a new TSV file in the specified output directory.
+
+    Args:
+        inpath (str): Path to the input TSV file containing nucleotide sequences.
+        outdir (str): Directory to save the translated output files.
+        reference_dir (str): Directory with reference for igblast
+    """
+    data = pd.read_csv(inpath, sep="\t")
+    out_fasta = os.path.join(outdir, os.path.splitext(os.path.basename(inpath))[0]+".fasta")
+    out_igblast = os.path.join(outdir, os.path.splitext(os.path.basename(inpath))[0]+"_igblast.tsv")
+    out_translated = os.path.join(outdir, os.path.splitext(os.path.basename(inpath))[0]+"_translated.tsv")
+
+    # Write out FASTA file
+    with open(out_fasta, "w") as f:
+        for _, row in data.iterrows():
+            f.write(">" + row["sequence_id"] + "\n")
+            f.write(row["sequence"] + "\n")
+
+    # Run IgBlast on FASTA
+    command_igblastn = ["igblastn", "-germline_db_V", f"{reference_dir}/database/imgt_human_ig_v",
+           "-germline_db_D", f"{reference_dir}/database/imgt_human_ig_d",
+           "-germline_db_J", f"{reference_dir}/database/imgt_human_ig_j",
+           "-query", out_fasta,
+           "-organism", "human",
+           "-auxiliary_data", f"{reference_dir}/optional_file/human_gl.aux",
+           "-show_translation",
+           "-outfmt", "19",
+           "-out", out_igblast]
+    pipes = subprocess.Popen(command_igblastn, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = pipes.communicate()
+
+    if pipes.returncode != 0:
+        raise Exception(f"IgBlast failed with error code {pipes.returncode}. {stderr.decode('utf-8')}")
+
+    # Read IgBlast output
+    igblast_transl = pd.read_csv(out_igblast, sep="\t", usecols=["sequence_id","sequence_aa", "sequence_alignment_aa"])
+
+    # Remove IMGT gaps
+    sequence_vdj_aa = [ sa.replace("-","") for sa in igblast_transl["sequence_alignment_aa"]]
+    igblast_transl["sequence_vdj_aa"] = sequence_vdj_aa
+
+    # Merge and save the translated data with original data
+    data_transl = pd.merge(data, igblast_transl, on="sequence_id", how="left")
+    data_transl.to_csv(out_translated, sep="\t", index=False)
+
+    # Clean up
+    os.remove(out_fasta)
+    os.remove(out_igblast)
+
 def main():
     asci_art = "BCR EMBED\n"
     asci_art = r"""
