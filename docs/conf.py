e41445f4011afe1da4d693c4ab008697feabaffe
Mamie Wang <mamie.wang@yale.edu>
2024-06-06 17:11:50 -0400

    Added CLI tutorial notebook

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ import amulety
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinxcontrib.typer", "sphinx_mdinclude"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinxcontrib.typer", "sphinx_mdinclude", "nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
