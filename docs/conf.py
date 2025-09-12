42bcd6b69b317637a276c28501b355d81890ffef
Gisela Gabernet <gisela.gabernet@gmail.com>
2025-09-12 16:05:21 -0400

    rm source definition

--- docs/conf.py
+++ docs/conf.py
@@ -61,11 +61,6 @@ napoleon_use_rtype = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = [".rst", ".md"]
 
 # The master toctree document.
 master_doc = "index"
