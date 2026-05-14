c333300737aa676cce3ab7436ec808b41b7b0c61
Gisela Gabernet <gisela.gabernet@gmail.com>
2026-05-13 22:23:46 -0400

    fix versions

--- amulety/__init__.py
+++ amulety/__init__.py
@@ -1,6 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .amulety import embed_airr, translate_airr
 
-__version__ = "2.0"
+try:
+    __version__ = version("amulety")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
 
 __all__ = [
     "embed_airr",
